30eee00e59d823c992d958abf19b846773643c85
omarkmasry <masry4help@gmail.com>
2018-02-07 01:31:08 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	AOS.init({
+	  duration: 900,
+	});
+	var scroll = new SmoothScroll('a[href*="#"]');
+	$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+});
